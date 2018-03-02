7d72dd5338fb2997e15ca2defcfd1705c38af9ae
92301@sangfor.com <92301@sangfor.com>
2018-03-01 20:35:38 -0500

    backup

---
+++ test_dhcp_client.sh
@@ -0,0 +1,1 @@
+./resources/traffic_scripts/dhcp/check_dhcp_request_ack.py --server_mac 00:0c:29:62:1a:46 --server_ip 100.200.10.1 --client_ip 100.200.10.100 --client_mask 255.255.255.0 --lease_time 1800 --rx_if ens225
